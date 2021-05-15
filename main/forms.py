a8c9f8ab9512d62d74e8a1efc43a62b8a74bab00
Daniel Min <dmin@salesforce.com>
2021-05-14 22:07:29 -0400

    Updates Tree Remover form: - Makes use of the submodule to perform tree remover task.

--- main/forms.py
+++ main/forms.py
@@ -3,14 +3,15 @@ from django.utils.safestring import mark_safe
 
 
 class TreeRemoverForm(forms.Form):
-    dataflows = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}), required=False,
-                                label='Dataflows', help_text='Select one or more dataflows')
-    replacer = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': False}), required=False,
-                               label='Replacer Dataflow', help_text='Select one dataflow')
+    dataflow = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': False}), required=False,
+                               label='Dataflows', help_text='Select a dataflow')
+    replacers = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}), required=False,
+                                label='Replacer Dataflow', help_text='Select one or more replacers')
     registers = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 1,
-                                                             'placeholder': "List of sfdcRegisters, registers, "
-                                                                            "edgeMart nodes"}),
+                                                             'placeholder': "List Register Nodes"}),
                                 required=True,
                                 label=mark_safe(
                                     "List of <strong>sfdcRegisters, registers, edgeMart</strong> nodes"), )
+    name = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Output dataflow name'}))
     result = forms.JSONField(required=False, label='Result', initial=False)
--- main/views.py
+++ main/views.py
@@ -1,10 +1,12 @@
-from django.views import generic
 import os
-from libs.utils import current_datetime, relative_mkdir, expand_home
 
+from django.views import generic
 
 # Create your views here.
 import main.forms as forms
+from libs.tcrm_automation.tree_remover import tree_remover
+from libs.utils import byte_to_str, expand_home, str_to_json, current_datetime
+from pathlib import Path
 
 
 class Home(generic.TemplateView):
@@ -23,17 +25,36 @@ class TreeRemover(generic.FormView):
     def post(self, request, *args, **kwargs):
         form_class = self.get_form_class()
         form = form_class(request.POST)
-        dataflows = request.FILES.getlist('dataflows')
-        replacer = request.FILES.getlist('replacer')
 
         if form.is_valid():
-            # print(form.cleaned_data.get('registers'))
-            # print(form.cleaned_data.get('result'))
-            today = current_datetime(add_time=False)
-            path = expand_home(f'~/Git/dataflow-json-sync/{today}')
-            if not os.path.isdir(path):
-                os.mkdir(path)
+            dataflow = request.FILES.getlist('dataflow')
+            replacers = request.FILES.getlist('replacer')
 
+            _dataflow = str_to_json(byte_to_str(dataflow[0].read()))
+            _replacers = [str_to_json(byte_to_str(replacer[0].read())) for replacer in replacers]
+
+            today = current_datetime(add_time=False)
+            outdir = os.path.join(Path(__file__).resolve().parent.parent, f'libs/tcrm_automation/{today}')
+            if not os.path.isdir(outdir):
+                os.makedirs(outdir)
+            _output = os.path.join(outdir, form.cleaned_data['name'].rstrip())
+
+            tree_remover(dataflow=_dataflow, replacers=_replacers,
+                         registers=[register.rstrip() for register in form.cleaned_data['registers'].split('\n')],
+                         output=_output)
+
+            original = request.FILES['dataflow'].temporary_file_path().replace(' ', "\\ ")
+            print('Temporally uploaded at:', original)
+            diff_command = f"python diff2HtmlCompare.py -s {original} {_output}"
+            cur_dir_tmp = "_CUR_DIR_TMP_"
+            _cmd_queue = [
+                F"export {cur_dir_tmp}=$(pwd)",
+                "cd libs/diff2HtmlCompare",
+                diff_command,
+                f"cd ${cur_dir_tmp}",
+                f"unset {cur_dir_tmp}"
+            ]
+            os.system(" && ".join(_cmd_queue))
 
             return self.form_valid(form)
         else:
