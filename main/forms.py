e9d350e03dbfed391f8f946f5b54b4bb7661c0e8
Daniel Min <dmin@salesforce.com>
2021-05-17 15:03:41 -0400

    Adds new field to TreeRemoverForm. - extract: boolean field whether to extract or just remove.

--- main/forms.py
+++ main/forms.py
@@ -3,10 +3,9 @@ from django.utils.safestring import mark_safe
 
 
 class TreeRemoverForm(forms.Form):
+    # Common fields
     dataflow = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': False}), required=False,
                                label='Dataflows', help_text='Select a dataflow')
-    replacers = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}), required=False,
-                                label='Replacer Dataflow', help_text='Select one or more replacers')
     registers = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 1,
                                                              'placeholder': "List Register Nodes"}),
                                 required=True,
@@ -15,4 +14,10 @@ class TreeRemoverForm(forms.Form):
     name = forms.CharField(
         widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Output dataflow name',
                                       'value': 'UpdatedDataflow.json'}))
-    result = forms.JSONField(required=False, label='Result', initial=False)
+
+    # For tree removers
+    replacers = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}), required=False,
+                                label='Replacer Dataflow', help_text='Select one or more replacers')
+
+    # For tree extractor
+    extract = forms.BooleanField(required=False)
