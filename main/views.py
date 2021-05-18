4d31fa9364cb1e9002f74f2fd4f470b9b8638f51
Daniel Min <dmin@salesforce.com>
2021-05-17 21:40:09 -0400

    Uses the new tree extractor interactor in views.py

--- main/views.py
+++ main/views.py
@@ -1,13 +1,9 @@
-import os
-
 from django.views import generic
 
 # Create your views here.
 import main.forms as forms
-from libs.tcrm_automation.tree_remover import tree_remover
-from libs.utils import byte_to_str, expand_home, str_to_json, current_datetime
-from pathlib import Path
-from .interactors.dataflow_tree_manager import TreeRemoverInteractor
+from libs.utils import byte_to_str, str_to_json
+from .interactors.dataflow_tree_manager import TreeExtractorInteractor, TreeRemoverInteractor
 
 
 class Home(generic.TemplateView):
@@ -23,6 +19,13 @@ class TreeRemover(generic.FormView):
     form_class = forms.TreeRemoverForm
     success_url = '/tree-remover/'
 
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['default_title'] = "Tree Remover"
+        return context
+
     def post(self, request, *args, **kwargs):
         form_class = self.get_form_class()
         form = form_class(request.POST)
@@ -37,13 +40,12 @@ class TreeRemover(generic.FormView):
             _dataflow = str_to_json(byte_to_str(dataflow[0].read()))
             _replacers = [str_to_json(byte_to_str(replacer[0].read())) for replacer in replacers]
 
-
-
             try:
                 if not extract:
                     _ = TreeRemoverInteractor.call(dataflow=_dataflow, replacers=_replacers, registers=registers,
                                                    name=name, request=request)
-
+                else:
+                    _ = TreeExtractorInteractor.call(dataflow=_dataflow, registers=registers, output_filename=name)
             except RuntimeError as rt_e:
                 print(rt_e)
 
