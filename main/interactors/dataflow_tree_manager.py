2813478ceab68a0436b3e51ba8bdb570405fa202
Daniel Min <dmin@salesforce.com>
2021-05-17 21:39:37 -0400

    Implements interactor for tree_remover.py.

--- main/interactors/dataflow_tree_manager.py
+++ main/interactors/dataflow_tree_manager.py
@@ -3,19 +3,33 @@ from pathlib import Path
 
 from libs.interactor.interactor import Interactor
 from libs.tcrm_automation.tree_remover import tree_remover
+from libs.tcrm_automation.tree_extractor import tree_extractor
 from libs.utils import current_datetime
+import platform as pf
+from appscript import *
+
+
+def _prepare_output_directory(filename: str = None, allow_empty_name: bool = False):
+    if not allow_empty_name and not filename:
+        raise NameError("Missing file name")
+
+    # Prepare the outout directory
+    today = current_datetime(add_time=False)
+    outdir = os.path.join(Path(__file__).resolve().parent.parent.parent, f'libs/tcrm_automation/{today}')
+
+    if not os.path.isdir(outdir):
+        os.makedirs(outdir)
+
+    if filename:
+        outdir = os.path.join(outdir, filename)
+
+    return outdir
 
 
 class TreeRemoverInteractor(Interactor):
     def run(self):
-        # Prepare the outout directory
-        today = current_datetime(add_time=False)
-        outdir = os.path.join(Path(__file__).resolve().parent.parent.parent, f'libs/tcrm_automation/{today}')
-
-        if not os.path.isdir(outdir):
-            os.makedirs(outdir)
-
-        _output = os.path.join(outdir, self.context.name.rstrip())
+        # Get output directory path
+        _output = _prepare_output_directory(filename=self.context.name.rstrip())
 
         # Execute removal
         tree_remover(dataflow=self.context.dataflow, replacers=self.context.replacers, registers=self.context.registers,
@@ -33,3 +47,17 @@ class TreeRemoverInteractor(Interactor):
             f"unset {cur_dir_tmp}"
         ]
         os.system(" && ".join(_cmd_queue))
+
+
+class TreeExtractorInteractor(Interactor):
+    def run(self):
+        output = _prepare_output_directory(allow_empty_name=True)
+        dataflow = self.context.dataflow
+        filename = self.context.output_filename
+        registers = self.context.registers
+
+        tree_extractor(dataflow=dataflow, registers=registers, output_dir=output, output_filename=filename)
+
+        if pf.system() == "Darwin":
+
+            app("Finder").reveal(mactypes.Alias(output).alias)
