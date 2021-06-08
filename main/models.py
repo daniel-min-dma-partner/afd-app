d8d0ae61beebcd2f23eb87193277a71e7867f795
Daniel Min <dmin@salesforce.com>
2021-06-08 10:40:17 -0400

    Adds SFDC Env model into admin page.

--- main/models.py
+++ main/models.py
@@ -1,3 +1,5 @@
+import copy
+
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -6,21 +8,25 @@ from django.db import models
 
 
 class SalesforceEnvironment(models.Model):
-    ENVIRONMENT_CHOICE = (
+    _ENVIRONMENT_CHOICE = (
         ('https://test.salesforce.com', 'Sandbox'),
         ('https://login.salesforce.com', 'Production'),
     )
 
-    client_key = models.CharField(max_length=128, help_text='', null=False, blank=False, default='', unique=True)
-    client_secret = models.CharField(max_length=128, help_text='', null=False, blank=False, default='', unique=True)
-    client_username = models.CharField(max_length=128, help_text='', null=False, blank=True, default='', unique=True)
-    client_password = models.CharField(max_length=128, help_text='', null=False, blank=False, default='', unique=True)
-    environment = models.CharField(max_length=28, help_text='', null=False, blank=False, choices=ENVIRONMENT_CHOICE,
+    client_key = models.CharField(max_length=128, help_text='', null=False, blank=False, default='')
+    client_secret = models.CharField(max_length=128, help_text='', null=False, blank=False, default='')
+    client_username = models.CharField(max_length=128, help_text='', null=False, blank=True, default='')
+    client_password = models.CharField(max_length=128, help_text='', null=False, blank=False, default='')
+    environment = models.CharField(max_length=28, help_text='', null=False, blank=False, choices=_ENVIRONMENT_CHOICE,
                                    default='https://test.salesforce.com')
-    name = models.CharField(max_length=128, help_text='', null=False, blank=False, default='', unique=True)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=128, help_text='', null=False, blank=False, default='')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
         constraints = [
             models.UniqueConstraint(fields=['user', 'environment', 'name'], name='unique_user_env_name')
         ]
+
+    @classmethod
+    def environment_choice(cls):
+        return copy.deepcopy(cls._ENVIRONMENT_CHOICE)
