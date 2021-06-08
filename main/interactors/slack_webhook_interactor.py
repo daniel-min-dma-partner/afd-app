e275f2954a8ebcebf45f13aa0954d13ff277e0d3
Daniel Min <dmin@salesforce.com>
2021-06-08 17:58:25 -0400

    Adds better message template for approval.

--- main/interactors/slack_webhook_interactor.py
+++ main/interactors/slack_webhook_interactor.py
@@ -1,34 +1,61 @@
 import copy
+import json
 
 from libs.interactor.interactor import Interactor
 
 _SLACK_MESSAGE_PAYLOAD_TEMPLATE = {
-    "blocks": [
+    "attachments": [
         {
-            "type": "section",
-            "text": {
-                "type": "mrkdwn",
-                "text": "You have a new request for approval:\n*<{{case_url}}|{{case_title}}>*"
-            }
-        },
-        {
-            "type": "section",
-            "fields": [
+            "blocks": [
                 {
-                    "type": "mrkdwn",
-                    "text": "*Description:*\n{{case_description}}"
+                    "type": "header",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "{{case_header}}",
+                        "emoji": True
+                    }
                 },
                 {
-                    "type": "mrkdwn",
-                    "text": "*Business Justification?:*\n{{case_business_justification}}"
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "{{case_description}}"
+                    },
+                    "accessory": {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "Link",
+                            "emoji": True
+                        },
+                        "value": "case_url",
+                        "url": "{{case_url}}",
+                        "action_id": "button-action"
+                    }
                 },
                 {
-                    "type": "mrkdwn",
-                    "text": "*Manager Approval?:*\n{{case_manager_approval}}"
+                    "type": "divider"
                 },
                 {
-                    "type": "mrkdwn",
-                    "text": "*Manager Name:*\n{{case_manager_name}}"
+                    "type": "section",
+                    "fields": [
+                        {
+                            "type": "mrkdwn",
+                            "text": "*Business Justification?*\n{{case_business_justification}}"
+                        },
+                        {
+                            "type": "mrkdwn",
+                            "text": "*Manager Approval?*\n{{case_manager_approval}}"
+                        },
+                        {
+                            "type": "mrkdwn",
+                            "text": "*Manager Name:*\n{{case_manager_name}}"
+                        },
+                        {
+                            "type": "mrkdwn",
+                            "text": "*Submiter:*\n{{submitter}}"
+                        }
+                    ]
                 }
             ]
         }
@@ -51,23 +78,29 @@ class SlackMessagePushInteractor(Interactor):
         manager_name = values['case-manager-name']
         url = values['case-url']
         case_number = values['case-number']
+        submitter = values['submitter']
 
         payload = copy.deepcopy(_SLACK_MESSAGE_PAYLOAD_TEMPLATE)
 
-        payload['blocks'][0]['text']['text'] = payload['blocks'][0]['text']['text'].replace('{{case_url}}', url)
-        payload['blocks'][0]['text']['text'] = payload['blocks'][0]['text']['text'].replace('{{case_title}}',
-                                                                                            f"Case #{case_number}")
+        payload = json.loads(json.dumps(payload)
+                             .replace('{{case_header}}', f"Case #{case_number}")
+                             .replace('{{case_description}}', f"_{description}_")
+                             .replace('{{case_url}}', url)
+                             .replace('{{case_business_justification}}', self._set_icon(business_justif))
+                             .replace('{{case_manager_approval}}', self._set_icon(manager_approval))
+                             .replace('{{case_manager_name}}', manager_name if manager_name else ":warning:")
+                             .replace('{{submitter}}', submitter))
 
-        payload['blocks'][1]['fields'] = [
-            {
-                "type": "mrkdwn",
-                "text": field['text']
-                    .replace('{{case_description}}', description)
-                    .replace('{{case_business_justification}}', str(business_justif))
-                    .replace('{{case_manager_approval}}', str(manager_approval))
-                    .replace('{{case_manager_name}}', manager_name)
-            }
-            for field in payload['blocks'][1]['fields']
-        ]
+        if not submitter:
+            del payload['attachments'][0]['blocks'][3]['fields'][3]
 
         return payload
+
+    @classmethod
+    def _set_icon(cls, bool_val: bool):
+        _icon_map = {
+            True: ":check:",
+            False: ":x:"
+        }
+
+        return _icon_map[bool_val]
