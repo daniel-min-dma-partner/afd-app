c4b137ca9941ebf046c7165aff18d28d5129113f
Daniel Min <dmin@salesforce.com>
2021-07-22 16:35:10 -0400

    Grops unread notis first and then read notis.

--- main/context_processors.py
+++ main/context_processors.py
@@ -2,5 +2,6 @@ from main.models import Notifications
 
 
 def show_notifications(request):
-    lst = Notifications.objects.filter(user_id=request.user.pk).order_by('-created_at').all()
-    return {'notifications': lst}
+    lst = Notifications.objects.filter(user_id=request.user.pk).order_by('status', '-created_at')
+    print(lst.query)
+    return {'notifications': lst.all()}
