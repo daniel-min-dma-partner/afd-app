4bfebed59d4646b4030364588fe3819608281fca
Daniel Min <dmin@salesforce.com>
2021-07-23 17:06:55 -0400

    Deletes all 'retrieve' folder before download dataflows.

--- main/interactors/download_dataflow_interactor.py
+++ main/interactors/download_dataflow_interactor.py
@@ -1,14 +1,19 @@
 import os
+import shutil
 
+from core.settings import BASE_DIR
 from libs.amt_helpers import generate_build_file, generate_package
 from libs.interactor.interactor import Interactor
-from core.settings import BASE_DIR
 
 
 class DownloadDataflowInteractor(Interactor):
     def run(self):
         _exc = None
         try:
+            # Deletes first the retrieve folder.
+            if os.path.isdir(f"{BASE_DIR}/ant/{self.context.user.username}/retrieve"):
+                shutil.rmtree(f"{BASE_DIR}/ant/{self.context.user.username}/retrieve")
+
             generate_build_file(self.context.model, self.context.user)
 
             members = [f"<members>{dataflow_name}</members>" for dataflow_name in self.context.dataflow]
