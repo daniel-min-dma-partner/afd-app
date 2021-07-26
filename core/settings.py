4e88595729119ef69e1fd4b7e99f53c975ba226c
Daniel Min <dmin@salesforce.com>
2021-07-26 14:10:43 -0400

    Adds whitenoise in the INSTALLED_APPS list.

--- core/settings.py
+++ core/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'django_extensions',
     'libs.interactor.interactor',
     'rest_framework',
+    'whitenoise.runserver_nostatic',
 
     # Created apps
     # 'chat',
@@ -218,6 +219,9 @@ STATICFILES_DIRS = [
     jsl_static_dir,
 ]
 
+# Static file handler for Heroku
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # File Upload managers
 FILE_UPLOAD_HANDLERS = [
     # "django.core.files.uploadhandler.MemoryFileUploadHandler",
@@ -248,4 +252,3 @@ SALESFORCE_INSTANCE_URLS = {
 django_heroku.settings(locals())
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
