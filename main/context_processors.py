d6caf5952596c0c74aa785366e7a4e70b5413e54
Daniel Min <dmin@salesforce.com>
2021-07-26 09:48:20 -0400

    Removes unneeded print.

--- main/context_processors.py
+++ main/context_processors.py
@@ -3,5 +3,4 @@ from main.models import Notifications
 
 def show_notifications(request):
     lst = Notifications.objects.filter(user_id=request.user.pk).order_by('status', '-created_at')
-    print(lst.query)
     return {'notifications': lst.all()}
