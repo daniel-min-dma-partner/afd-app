020f4d649bf9d33c9446d56ef17627d9c98e7621
Daniel Min <dmin@salesforce.com>
2021-07-26 17:00:14 -0400

    Removes downloading files in a ephimeral fs.

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,13 +1,3 @@
-echo "======== DOWNLOADING ANT ========"
-echo ""
-
-curl https://downloads.apache.org/ant/binaries/apache-ant-1.10.11-bin.tar.gz --output ant/apache-ant.bin.tar.gz
-tar -xvf ant/apache-ant.bin.tar.gz -C ant
-ls -al ant | grep ant
-ls -al ant/apache-ant-1.10.11
-echo "======== ANT DOWNLOADED ========"
-echo ""
-
 echo "======== SHOWING 'PATH' ========"
 echo ""
 echo $PATH
