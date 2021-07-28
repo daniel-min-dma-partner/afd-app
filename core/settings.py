2892b62a1c0ad9cf8873dfca9288d4069a682302
Daniel Min <dmin@salesforce.com>
2021-07-28 11:17:47 -0400

    Implements local_settings.py

--- core/settings.py
+++ core/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-wi5%3e1_fpxq+fm8sowdg0^(0vz*qv0oryh3ww+adav$+v$e4%'
-SECRET_KEY = env.str('SECRET_KEY', default='django-insecure-wi5%3e1_fpxq+fm8sowdg0^(0vz*qv0oryh3ww+adav$+v$e4%')
+SECRET_KEY = env.str('SECRET_KEY', default='')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = False
@@ -219,9 +219,6 @@ STATICFILES_DIRS = [
     jsl_static_dir,
 ]
 
-# Static file handler for Heroku
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-
 # File Upload managers
 FILE_UPLOAD_HANDLERS = [
     # "django.core.files.uploadhandler.MemoryFileUploadHandler",
@@ -248,7 +245,15 @@ SALESFORCE_INSTANCE_URLS = {
     'Production': 'https://login.salesforce.com',
 }
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
-prod_db = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+try:
+    from core.local_settings import *
+except ImportError as e:
+    # Activate Django-Heroku.
+    django_heroku.settings(locals())
+
+    # Configure database for Heroku
+    prod_db = dj_database_url.config(conn_max_age=500)
+    DATABASES['default'].update(prod_db)
+
+    # Static file handler for Heroku
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
