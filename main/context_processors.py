44cd2c4d842b2aab77f6df8aaaf474d8ff406cfd
dmin <dmin@salesforce.com>
2021-08-26 12:22:52 -0400

    Adds upload notification list to context processor.

--- main/context_processors.py
+++ main/context_processors.py
@@ -1,6 +1,11 @@
-from main.models import Notifications
+from main.models import Notifications, UploadNotifications as UpNotifs
 
 
 def show_notifications(request):
     lst = Notifications.objects.filter(user_id=request.user.pk).order_by('status', '-created_at')
-    return {'notifications': lst.all()}
+    up_notifs = UpNotifs.objects.filter(user=request.user).order_by('status', '-created_at')
+
+    return {
+        'notifications': lst.all(),
+        'upload_notifications': up_notifs.all()
+    }
