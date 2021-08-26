ba18ab70d143634db6a150b88128de12b5623fb2
dmin <dmin@salesforce.com>
2021-08-26 13:20:43 -0400

    Fixes error caused by anonymous user in context_processors.py

--- main/context_processors.py
+++ main/context_processors.py
@@ -3,7 +3,12 @@ from main.models import Notifications, UploadNotifications as UpNotifs
 
 def show_notifications(request):
     lst = Notifications.objects.filter(user_id=request.user.pk).order_by('status', '-created_at')
-    up_notifs = UpNotifs.objects.filter(user=request.user).order_by('status', '-created_at')
+    up_notifs = UpNotifs.objects.filter(user_id=request.user.pk).order_by('status', '-created_at')
+
+    ### Information ###
+    # If you use `user=request.user` and you are not logged in, then it will throws an exception.
+    # The reason is that the user object of the request object is not iterable when it is an anonymous.
+    # Use `user_id=request.user.pk`
 
     return {
         'notifications': lst.all(),
