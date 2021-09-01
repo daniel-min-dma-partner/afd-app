f619c15044e8b9a51c412eaa63bacd13cc6b45e6
dmin <dmin@salesforce.com>
2021-09-01 17:20:56 -0400

    Also stores dataflow not deprecated or with errors. - Attaches the status and a message.

--- main/interactors/deprecate_fields_interactor.py
+++ main/interactors/deprecate_fields_interactor.py
@@ -33,6 +33,7 @@ class FieldDeprecatorInteractor(Interactor):
         _original_df_name = None
         _error_notification = False
 
+        deprecation_detail = None
         deprecation_model = None
         deprecation_models = []
         not_deprecated_dfs = []
@@ -85,6 +86,26 @@ class FieldDeprecatorInteractor(Interactor):
                     try:
                         with open(os.path.join(deprecation_dir, f"{df_name}.log"),
                                   'w') as log_file:
+
+                            if not deprecation_model:
+                                deprecation_model = DataflowDeprecation()
+                                deprecation_model.user = user
+                                deprecation_model.name = name
+                                deprecation_model.salesforce_org = org
+                                deprecation_model.case_url = case_url
+                                deprecation_model.sobjects = "|".join(objects)
+                                deprecation_model.fields = "|".join(fields)
+                                deprecation_model.save()
+                                deprecation_model.refresh_from_db()
+
+                            deprecation_detail = DeprecationDetails()
+                            deprecation_detail.file_name = f"[{today}] {df_name}"
+                            deprecation_detail.original_dataflow = _original
+                            deprecation_detail.deprecated_dataflow = _original
+                            deprecation_detail.meta = _field_md_original
+                            deprecation_detail.deprecation = deprecation_model
+                            deprecation_detail.save()
+
                             node_list = get_nodes_by_action(df=dataflow, action=['sfdcDigest', 'digest', 'edgemart'])
                             json_modified = perform_deprecation(df=dataflow, fieldlist=field_md,
                                                                 node_list=node_list, df_name=df_name,
@@ -94,27 +115,19 @@ class FieldDeprecatorInteractor(Interactor):
                             equal = json.dumps(_original) == json.dumps(json_modified)
 
                             if not equal:
-                                if not deprecation_model:
-                                    deprecation_model = DataflowDeprecation()
-                                    deprecation_model.user = user
-                                    deprecation_model.name = name
-                                    deprecation_model.salesforce_org = org
-                                    deprecation_model.case_url = case_url
-                                    deprecation_model.sobjects = "|".join(objects)
-                                    deprecation_model.fields = "|".join(fields)
-                                    deprecation_model.save()
-                                    deprecation_model.refresh_from_db()
-
-                                deprecation_detail = DeprecationDetails()
-                                deprecation_detail.file_name = f"[{today}] {df_name}"
-                                deprecation_detail.original_dataflow = _original
                                 deprecation_detail.deprecated_dataflow = json_modified
-                                deprecation_detail.meta = _field_md_original
-                                deprecation_detail.deprecation = deprecation_model
+                                deprecation_detail.status = DeprecationDetails.SUCCESS
                                 deprecation_detail.save()
                             else:
+                                deprecation_detail.status = DeprecationDetails.NO_DEPRECATION
+                                deprecation_detail.message = "The dataflow doesn't have any reference to the listed object/fields."
+                                deprecation_detail.save()
                                 not_deprecated_dfs.append(_original_df_name)
                     except Exception as e:
+                        deprecation_detail.status = DeprecationDetails.ERROR
+                        deprecation_detail.message = str(e)
+                        deprecation_detail.save()
+
                         _error_notification = True
                         notif = Notifications()
                         notif.user = self.context.user
