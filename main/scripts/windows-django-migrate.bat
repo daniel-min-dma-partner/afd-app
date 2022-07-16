850ff514eafa081d082b686bb778f37355176fd9
HeanGee <dpark875@gmail.com>
2022-07-16 10:46:59 -0400

    Add cmd to create default env.

--- main/scripts/windows-django-migrate.bat
+++ main/scripts/windows-django-migrate.bat
@@ -2,4 +2,4 @@ python .\manage.py migrate
 python .\manage.py initadmin
 python .\manage.py loaddata main\fixtures\system-parameter.json
 python .\manage.py collectstatic --no-input --clear
-python .\manage.py runserver 0.0.0.0:8000
+python .\manage.py init-tcrm-connection
